fb993857ea354c54f5dc895afbc5973649bdd28a
Stefano <stefanop21@outlook.es>
2024-03-12 21:03:08 -0500

    update(src/auth/pages/LoginPage.jsx): Diseño del LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,71 @@
+import './LoginPage.css';
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-md-6 login-form-1">
+          <h3>Iniciar sesión</h3>
+          <form>
+            <div className="form-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Correo electrónico"
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input type="submit" className="btnSubmit" value="Ingresar" />
+            </div>
+          </form>
+        </div>
+
+        <div className="col-md-6 login-form-2">
+          <h3>Crear cuenta</h3>
+          <form>
+            <div className="form-group mb-2">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre de usuario"
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Correo electrónico"
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+              />
+            </div>
+
+            <div className="form-group mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repita la contraseña"
+              />
+            </div>
+
+            <div className="form-group mb-3">
+              <input type="submit" className="btnSubmit" value="Registrarse" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
