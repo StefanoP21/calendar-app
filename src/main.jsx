3faba33d6858b1e77d5a0a020771d416d6666149
Stefano <stefanop21@outlook.es>
2024-03-13 17:57:09 -0500

    config(src/main.jsx): Chakra UI Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { CalendarApp } from './CalendarApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CalendarApp />
+    <ChakraProvider>
+      <CalendarApp />
+    </ChakraProvider>
   </React.StrictMode>
 );
