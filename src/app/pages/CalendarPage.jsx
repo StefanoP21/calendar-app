0158f488632556d4b1f682f8ef94e7989b5ad2f0
Stefano <stefanop21@outlook.es>
2024-03-14 21:58:53 -0500

    update(src/app/pages/CalendarPage.jsx): Implementación del hook useCalendarStore

--- src/app/pages/CalendarPage.jsx
+++ src/app/pages/CalendarPage.jsx
@@ -1,59 +1,21 @@
-import { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { addHours } from 'date-fns';
 import { NavBar, CalendarEvent, CalendarModal } from '../';
 
 import { calendarLocalizer, getMessages } from '../../helpers';
-import { Button } from '@chakra-ui/react';
-import { useUiStore } from '../../hooks';
 
-const events = [
-  {
-    title: 'My event',
-    note: 'This is my event',
-    start: new Date(),
-    end: addHours(new Date(), 2),
-    bgColor: '#0073ff',
-    user: {
-      _id: '123',
-      name: 'John Doe',
-    },
-  },
-];
+import { useCalendarStore } from '../../hooks';
 
 export const CalendarPage = () => {
-  const { openDateModal } = useUiStore();
-  const [lastView, setLastView] = useState(
-    localStorage.getItem('lastView') || 'month'
-  );
-
-  const eventStyleGetter = (event, start, end, isSelected) => {
-    const style = {
-      backgroundColor: event.bgColor,
-      borderRadius: '0px',
-      opacity: 0.8,
-      color: 'white',
-    };
-
-    return {
-      style,
-    };
-  };
-
-  const onDoubleClick = (e) => {
-    openDateModal();
-  };
-
-  const onSelect = (e) => {
-    console.log({ onSelect: e });
-  };
-
-  const onViewChange = (e) => {
-    localStorage.setItem('lastView', e);
-    setLastView(e);
-  };
+  const {
+    events,
+    lastView,
+    eventStyleGetter,
+    onDoubleClick,
+    onSelect,
+    onViewChange,
+  } = useCalendarStore();
 
   return (
     <>
