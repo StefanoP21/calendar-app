bee14af1fa22e409c009b392febbb429dc2f1a11
Stefano <stefanop21@outlook.es>
2024-03-22 21:28:51 -0500

    update(src/auth/pages/LoginPage.jsx): Implementación del useForm

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,19 +1,37 @@
 import './styles.css';
-
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks';
+
+const loginFormFields = {
+  email: '',
+  password: '',
+};
 
 export const LoginPage = () => {
+  const { email, password, onInputChange } = useForm(loginFormFields);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      email,
+      password,
+    });
+  };
+
   return (
     <div className="container login-container">
       <div className="row justify-content-center">
         <div className="col-lg-8 login-form-1">
           <h3>Iniciar sesión</h3>
-          <form>
+          <form onSubmit={onFormSubmit}>
             <div className="form-group mb-3">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Correo electrónico"
+                name="email"
+                value={email}
+                onChange={onInputChange}
               />
             </div>
             <div className="form-group mb-3">
@@ -21,6 +39,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="password"
+                value={password}
+                onChange={onInputChange}
               />
             </div>
             <div className="form-group mb-3">
