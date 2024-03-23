9589535725292bc7edb071d61b744d1966972a6c
Stefano <stefanop21@outlook.es>
2024-03-22 21:29:22 -0500

    update(src/auth/pages/RegisterPage.jsx): Implementación del useForm

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,18 +1,42 @@
-import { Link } from 'react-router-dom';
 import './styles.css';
+import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks';
+
+const registerFormFields = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
 
 export const RegisterPage = () => {
+  const { name, email, password, confirmPassword, onInputChange } =
+    useForm(registerFormFields);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  };
+
   return (
     <div className="container login-container">
       <div className="row justify-content-center">
         <div className="col-lg-8 login-form-1">
           <h3>Crear cuenta</h3>
-          <form>
+          <form onSubmit={onFormSubmit}>
             <div className="form-group mb-3">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Nombre de usuario"
+                name="name"
+                value={name}
+                onChange={onInputChange}
               />
             </div>
             <div className="form-group mb-3">
@@ -20,6 +44,9 @@ export const RegisterPage = () => {
                 type="email"
                 className="form-control"
                 placeholder="Correo electrónico"
+                name="email"
+                value={email}
+                onChange={onInputChange}
               />
             </div>
             <div className="form-group mb-3">
@@ -27,6 +54,9 @@ export const RegisterPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="password"
+                value={password}
+                onChange={onInputChange}
               />
             </div>
             <div className="form-group mb-3">
@@ -34,6 +64,9 @@ export const RegisterPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Repita la contraseña"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={onInputChange}
               />
             </div>
             <div className="form-group mb-3">
