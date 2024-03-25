1ac3889f5c9efd306eb7d41666a21cd877f3da6e
Stefano <stefanop21@outlook.es>
2024-03-25 17:17:43 -0500

    new(src/context/auth/authSlice.js): Reducers onLogout y clearError

--- src/context/auth/authSlice.js
+++ src/context/auth/authSlice.js
@@ -21,8 +21,18 @@ export const authSlice = createSlice({
       state.user = payload;
       state.errorMessage = undefined;
     },
+
+    onLogout: (state, { payload }) => {
+      state.status = 'not-authenticated';
+      state.user = {};
+      state.errorMessage = payload;
+    },
+
+    clearError: (state) => {
+      state.errorMessage = undefined;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { onChecking, onLogin } = authSlice.actions;
+export const { onChecking, onLogin, onLogout, clearError } = authSlice.actions;
