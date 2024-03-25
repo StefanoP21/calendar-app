3831d14d1207586fbcbf2c9ed77db1ccd96cf716
Stefano <stefanop21@outlook.es>
2024-03-25 17:18:34 -0500

    update(src/hooks/useAuthStore.js): Despachar onLogin, onLogout y clearError

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,16 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { calendarApi } from '../api';
+import { clearError, onChecking, onLogin, onLogout } from '../context';
 
 export const useAuthStore = () => {
   const { status, user, errorMessage } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const startLogin = async ({ email, password }) => {
+    dispatch(onChecking());
     try {
-      const resp = await calendarApi.post('/auth', { email, password });
-      console.log({ resp });
+      const { data } = await calendarApi.post('/auth', { email, password });
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
-      console.log({ error });
+      dispatch(onLogout('Usuario o contraseña incorrectos'));
+
+      setTimeout(() => {
+        dispatch(clearError());
+      }, 5000);
     }
   };
 
