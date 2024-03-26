7b9b128544acc558755842e6bb049d4ac6d7d8a8
Stefano <stefanop21@outlook.es>
2024-03-25 19:14:36 -0500

    update(src/auth/pages/): Diseño de formularios

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -26,38 +26,36 @@ export const LoginPage = () => {
 
   return (
     <div className="container login-container">
-      <div className="row justify-content-center">
-        <div className="col-lg-8 login-form-1">
-          <h3>Iniciar sesión</h3>
-          <form onSubmit={onFormSubmit}>
-            <div className="form-group mb-3">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Correo electrónico"
-                name="email"
-                value={email}
-                onChange={onInputChange}
-              />
-            </div>
-            <div className="form-group mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Contraseña"
-                name="password"
-                value={password}
-                onChange={onInputChange}
-              />
-            </div>
-            <div className="form-group mb-3">
-              <input type="submit" className="btnSubmit" value="Ingresar" />
-            </div>
-          </form>
-
-          <div className="form-group">
-            <Link to="/auth/register">¿No tienes una cuenta?</Link>
+      <div className="col-lg-8 login-form-1">
+        <h3>Iniciar sesión</h3>
+        <form onSubmit={onFormSubmit}>
+          <div className="form-group mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Correo electrónico"
+              name="email"
+              value={email}
+              onChange={onInputChange}
+            />
           </div>
+          <div className="form-group mb-3">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Contraseña"
+              name="password"
+              value={password}
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <input type="submit" className="btnSubmit" value="Ingresar" />
+          </div>
+        </form>
+
+        <div className="form-group">
+          <Link to="/auth/register">¿No tienes una cuenta?</Link>
         </div>
       </div>
     </div>
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -38,57 +38,55 @@ export const RegisterPage = () => {
 
   return (
     <div className="container login-container">
-      <div className="row justify-content-center">
-        <div className="col-lg-8 login-form-1">
-          <h3>Crear cuenta</h3>
-          <form onSubmit={onFormSubmit}>
-            <div className="form-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Nombre de usuario"
-                name="name"
-                value={name}
-                onChange={onInputChange}
-              />
-            </div>
-            <div className="form-group mb-3">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Correo electrónico"
-                name="email"
-                value={email}
-                onChange={onInputChange}
-              />
-            </div>
-            <div className="form-group mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Contraseña"
-                name="password"
-                value={password}
-                onChange={onInputChange}
-              />
-            </div>
-            <div className="form-group mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Repita la contraseña"
-                name="confirmPassword"
-                value={confirmPassword}
-                onChange={onInputChange}
-              />
-            </div>
-            <div className="form-group mb-3">
-              <input type="submit" className="btnSubmit" value="Registrarse" />
-            </div>
-          </form>
+      <div className="col-lg-8 login-form-1">
+        <h3>Crear cuenta</h3>
+        <form onSubmit={onFormSubmit}>
+          <div className="form-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Nombre de usuario"
+              name="name"
+              value={name}
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Correo electrónico"
+              name="email"
+              value={email}
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Contraseña"
+              name="password"
+              value={password}
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Repita la contraseña"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <input type="submit" className="btnSubmit" value="Registrarse" />
+          </div>
+        </form>
 
-          <Link to="/auth/login">¿Ya tienes una cuenta?</Link>
-        </div>
+        <Link to="/auth/login">¿Ya tienes una cuenta?</Link>
       </div>
     </div>
   );
