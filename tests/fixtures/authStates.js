bb0be155aa77b2d042fb983f516d660cb6071a3b
Stefano <stefanop21@outlook.es>
2024-03-28 20:05:40 -0500

    new(tests/fixtures/): Fixtures

---
+++ tests/fixtures/authStates.js
@@ -0,0 +1,22 @@
+import { testUserCredentials } from './testUser';
+
+export const initialState = {
+  status: 'checking',
+  user: {},
+  errorMessage: undefined,
+};
+
+export const authenticatedState = {
+  status: 'authenticated',
+  user: {
+    name: 'Test User',
+    uid: 'abc-123',
+  },
+  errorMessage: undefined,
+};
+
+export const notAuthenticatedState = {
+  status: 'not-authenticated',
+  user: {},
+  errorMessage: undefined,
+};
---
+++ tests/fixtures/testUser.js
@@ -0,0 +1,6 @@
+export const testUserCredentials = {
+  email: 'test@gmail.com',
+  password: '123456',
+  uid: '66036b50603abb2f0d2e9b31',
+  name: 'Test User',
+};
